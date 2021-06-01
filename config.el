54aa0a55c9f3d42aa87eb2a680baae9113bf4678
Eoin Carney <eoincarney0@gmail.com>
2021-06-01 23:03:30 +0900

    added variable pitch font

--- config.el
+++ config.el
@@ -1,7 +1,8 @@
 (setq user-full-name "Eoin Carney"
       user-mail-address "eoin@spool-five.com")
 
-(setq doom-font (font-spec :family "Source Code Pro" :size 20))
+(setq doom-font (font-spec :family "Source Code Pro" :size 20)
+      doom-variable-pitch-font (font-spec :family "Source Sans Variable"))
 
 (setq doom-theme 'doom-miramare)
 
