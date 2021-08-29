90ef2b85dab288382e8e68ce3417c37cc6689d4d
Eoin Carney <eoincarney0@gmail.com>
2021-08-29 13:49:02 +0900

    org roam tags

--- config.el
+++ config.el
@@ -182,7 +182,7 @@
       :desc "Daily Today"       "t"     'org-roam-dailies-find-today
       :desc "Daily Date"        "d"     'org-roam-dailies-goto-date)
 
-(setq org-roam-node-display-template "${title} ${tags}")
+(setq org-roam-node-display-template "${title:*} ${tags:30}") ;the format here is $(field-name:length). Including the 'length' integer causes the alignment of the tags to the right, ommitting it leaves them on the left.
 (advice-remove 'org-roam-capture--get-target #'org-roam-capture--get-if-new-target-a)
 
 (use-package! org-roam-bibtex
